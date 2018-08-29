10d1d65259151a425f38f1780fec973277f8825d
edmondye502 <edmondye502@users.noreply.github.com>
2018-08-29 16:00:43 -0700

    Update settings.py

--- edohub/settings.py
+++ edohub/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%^g%m@18)hz8m=7co5ijv!fxcg@g8%pdf47_qehrkq3wz6adqu'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
